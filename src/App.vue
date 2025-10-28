400e044e19722481a269bf6c08c3e011f586d7f6
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-10-28 15:53:52 -0500

    Add component ProductSection.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div>
     <Navbar />
     <Hero :theme="theme" />
+    <ProductsSection />
   </div>
 </template>
 
 <script setup>
 import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
+import ProductsSection from "./components/ProductsSection.vue";
 </script>
 
 <style lang="scss"></style>
