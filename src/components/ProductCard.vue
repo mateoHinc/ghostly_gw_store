b22b3cad982faa269184d80197af5a5065efeea8
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-01 14:43:28 -0500

    ProductCard.vue component, done!

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Product Card Component</h1>
+  <div class="position-relative h-100 card hover-rise">
+    <span
+      v-if="product.isNew"
+      class="top-0 position-absolute m-3 end-0 badge badge-new"
+      >New</span
+    >
+    <img :src="product.image" :alt="product.name" class="card-img-top" />
+    <div class="d-flex flex-column card-body">
+      <h5 class="mb-1 card-title">{{ product.name }}</h5>
+      <p
+        class="card-text small"
+        :class="theme === 'dark' ? 'text-white-50' : 'text-muted'"
+      >
+        {{ product.desc }}
+      </p>
+      <div class="d-flex align-items-center justify-content-between mt-auto">
+        <span class="fw-bold">$ {{ product.price }}</span>
+        <button
+          class="btn-outline-info btn btn-sm"
+          @click="$emit('add-to-cart', product)"
+        >
+          <i class="bi bi-plus-lg">Add</i>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const props = defineProps({ product: Object, theme: String });
 </script>
 
 <style></style>
