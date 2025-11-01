367906ecc4198ab81aa47bffc2452b8f2bbd103e
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-01 14:01:21 -0500

    Working component ProductsSection.vue

--- src/components/ProductsSection.vue
+++ src/components/ProductsSection.vue
@@ -1,4 +1,32 @@
-<template></template>
+<template>
+  <section id="products" class="py-5 container">
+    <div
+      class="d-flex flex-wrap align-items-end justify-content-between gap-3 mb-3"
+    >
+      <div>
+        <h2 class="m-0 section-title display-5">Featured Collection</h2>
+        <div class="mt-2 divider"></div>
+      </div>
+      <div class="d-flex gap-2">
+        <button
+          v-for="c in categories"
+          :key="c"
+          class="filter-btn text-uppercase btn btn-sm"
+          :class="{ active: selectedCategory === c }"
+          @click="$emit('update-category', c)"
+        >
+          {{ c }}
+        </button>
+      </div>
+    </div>
+
+    <div class="row g-4">
+      <div class="col-12 col-sm-6 col-lg-4" v-for="p in filtered" :key="p.id">
+        <h3>Product Card</h3>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {};
