c4c0824c905ec11e218a1c63282f58292a98a7a6
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-02 15:13:15 -0500

    Add component Newsletter.vue in App

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
       @add-to-cart="addToCart"
     />
     <About :theme="theme" />
+    <Newsletter :theme="theme" v-model:email="email" @subscribe="subscribe" />
   </div>
 </template>
 
@@ -21,6 +22,7 @@ import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 import ProductsSection from "./components/ProductsSection.vue";
 import About from "./components/About.vue";
+import Newsletter from "./components/Newsletter.vue";
 
 const cartCount = ref(0);
 const email = ref("");
@@ -28,14 +30,6 @@ const selectedCategory = ref("All");
 const categories = ["All", "T-shirts", "sweatshirts", "caps"];
 
 const products = ref(productsData);
-// const products = [
-//     { id: 1, name: 'Camiseta Drift Tee', price: 129000, category: 'Camisetas', isNew: true, desc: 'Algodón premium 240gsm, cuello reforzado.', image: 'https://images.unsplash.com/photo-1548883354-6db3da77e08e?q=80&w=1200&auto=format&fit=crop' },
-//     { id: 2, name: 'Hoodie Apex', price: 199000, category: 'Hoodies', isNew: false, desc: 'Polar liviano + capucha de 3 paneles.', image: 'https://images.unsplash.com/photo-1520974735194-5f39db52d1b2?q=80&w=1200&auto=format&fit=crop' },
-//     { id: 3, name: 'Jogger Impact', price: 189000, category: 'Joggers', isNew: true, desc: 'Rodillas reforzadas y bolsillo oculto.', image: 'https://images.unsplash.com/photo-1594751543129-6701ad444259?q=80&w=1200&auto=format&fit=crop' },
-//     { id: 4, name: 'Gorra RailCap', price: 79000, category: 'Accesorios', isNew: false, desc: 'Ripstop, ajuste rápido.', image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1200&auto=format&fit=crop' },
-//     { id: 5, name: 'Camiseta Flow', price: 119000, category: 'Camisetas', isNew: false, desc: 'Secado rápido y costuras planas.', image: 'https://images.unsplash.com/photo-1512436991641-6745cdb1723f?q=80&w=1200&auto=format&fit=crop' },
-//     { id: 6, name: 'Hoodie Rail', price: 209000, category: 'Hoodies', isNew: true, desc: 'Tejido técnico y bolsillo canguro.', image: 'https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=1200&auto=format&fit=crop' }
-//   ]
 
 function addToCart() {
   cartCount.value++;
@@ -48,6 +42,12 @@ function showCartToast() {
 
   const t = new window.bootstrap.Toast(toastEl, { delay: 1800 });
   t.show();
+}
+
+function subscribe() {
+  if (!email.value) return;
+  alert(`¡Gracias! Te avisaremos a ${email.value}`);
+  email.value = "";
 }
 </script>
 
