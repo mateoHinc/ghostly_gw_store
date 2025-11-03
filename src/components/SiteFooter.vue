6cc1735a88fdecdd40ae95bd1f49bd133aafeef4
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-03 14:32:13 -0500

    Component SiteFooter.vue, done!

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,35 @@
-<template></template>
+<template>
+  <footer class="py-4 footer">
+    <div
+      class="d-flex flex-wrap alig-items-center justify-content-between gap-3 container"
+    >
+      <span
+        class="small"
+        :class="theme === 'dark' ? 'text-white-50' : 'text-muted'"
+        >© {{ year }} Ghostly_GW Clothes. All rights reserved.</span
+      >
+      <div class="d-flex gap-3">
+        <a href="#" :class="theme === 'dark' ? 'text-white-50' : 'text-dark'"
+          ><i class="bi bi-instagram"></i
+        ></a>
+        <a href="#" :class="theme === 'dark' ? 'text-white-50' : 'text-dark'"
+          ><i class="bi bi-youtube"></i
+        ></a>
+        <a href="#" :class="theme === 'dark' ? 'text-white-50' : 'text-dark'"
+          ><i class="bi bi-discord"></i
+        ></a>
+      </div>
+    </div>
+  </footer>
+</template>
 
-<script>
-export default {};
+<script setup>
+import { ref, onMounted } from "vue";
+const props = defineProps({ theme: String });
+const year = ref("");
+onMounted(() => {
+  year.value = String(new Date().getFullYear);
+});
 </script>
 
 <style></style>
